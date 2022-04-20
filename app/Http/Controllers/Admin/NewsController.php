83c73fee362182050ad0703611bb9cec6008c000
s-tajima <shiho.tajima1031@gmail.com>
2022-04-20 14:45:36 +0000

    課題提出13

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -4,17 +4,37 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\News;
 
 class NewsController extends Controller
 {
     //
-     public function add()
+  public function add()
   {
       return view('admin.news.create');
   }
+  
   public function create(Request $request)
   {
-      // admin/news/createにリダイレクトする
-      return redirect('admin/news/create');
-  }  
+        $this->validate($request, News::$rules);
+        $news = new News;
+        $form = $request->all();
+        
+        if(isset($form['image'])) {
+            $path = $request->file('image')->store('public/image');
+            $news->image_path = basename($path);
+        } else {
+            $news->image_path = null;
+        }
+        
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+         // フォームから送信されてきたimageを削除する
+         unset($form['image']);
+        // データベースに保存する
+        $news->fill($form);
+        $news->save();
+
+          return redirect('admin/news/create');
+  }
 }
