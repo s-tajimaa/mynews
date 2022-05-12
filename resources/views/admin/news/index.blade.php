58330d3b41ee03de917ae4d75b26534ccc48b341
s-tajima <shiho.tajima1031@gmail.com>
2022-05-12 12:15:41 +0000

    composer.jsonとcomposer.lockをherokuデプロイ用に最適化しました

--- resources/views/admin/news/index.blade.php
+++ resources/views/admin/news/index.blade.php
@@ -21,6 +21,16 @@
                             {{ csrf_field() }}
                             <input type="submit" class="btn btn-primary" value="検索">
                         </div>
+                        <div class="image">
+                           @if ($headline->image_path)
+                                <img src="{{ $headline->image_path }}">
+                           @endif
+                        <div class="image col-md-6 text-right mt-4">
+                           @if ($post->image_path)
+                                <img src="{{ $post->image_path }}">
+                           @endif
+                        </div>
+                        </div>
                     </div>
                 </form>
             </div>
